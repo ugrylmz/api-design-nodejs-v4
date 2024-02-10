39bebfb4f29c552a2c2380b59a40ba7df952b140
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-02-10 23:46:28 +0300

    feat(user.ts): add handlers for creating a new user and logging in a user The `createNewUser` function handles the creation of a new user by validating the user input, hashing the password, and creating a new user in the database using Prisma. If the validation fails, it returns a 400 status with the validation errors. If the creation is successful, it returns a 201 status with a JWT token.
    The `loginUser` function handles the login process by checking if the username and password are provided and meet the length requirements. It then finds the user in the database using Prisma and compares the provided password with the stored hashed password. If the login is successful, it returns a 200 status with a JWT token. If the username or password is invalid, it returns a 400 status with an appropriate error message.

---
+++ src/handlers/user.ts
@@ -0,0 +1,48 @@
+import prisma from '../db';
+import { hashPassword, createJWT, comparePassword } from '../modules/auth'
+import { validateUserInput } from '../utils/validation';
+
+export const createNewUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    const validation = validateUserInput(username, email, password);
+    console.log(validation);
+   if (!validation.isValid) {
+       return res.status(400).json({ errors: validation.errors });
+    }
+
+    try {
+        const hashedPassword = await hashPassword(password);
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password:hashedPassword 
+            }
+        });
+        const token = await createJWT(newUser);
+        res.status(201).json({ token });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message });
+    }
+}
+
+export const loginUser = async (req, res) => {
+    const { username, password } = req.body;
+    if(!username || !password) return res.status(400).json({ message: 'Username and password are required.' });
+    if(username.length>2 || password.length>8) return res.status(400).json({ message: 'Username must be at least 3 characters long and password must be at least 8 characters long.' });
+    const user = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    });
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid username or password.' });
+    }
+    const isValid = await comparePassword(password, user.password);
+    if (!isValid) {
+        return res.status(400).json({ message: 'Invalid username or password.' });
+    }
+    const token = await createJWT(user);
+    res.status(200).json({ token });
+}
