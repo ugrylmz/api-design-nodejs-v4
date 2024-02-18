77d873a235ad20c97a9e0d8027190c8ee8a822be
Uğur Yılmaz <ugur@uguryilmaz.dev>
2024-02-18 23:40:39 +0300

    feat(validations): add update validations for PUT and POST requests A new file, update.ts, has been created to handle validations for PUT and POST requests related to updates. The file includes validation rules for fields such as title, body, status, and version, ensuring that the data provided in the requests meets the specified criteria.

---
+++ src/validations/update.ts
@@ -0,0 +1,1 @@
+import {body, oneOf} from "express-validator";export const putUpdateValidation = [    body('title').optional(),    body('body').optional(),    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),    body('version').optional()];export const postUpdateValidation = [    body('title').exists().isString(),    body('body').exists().isString(),];
